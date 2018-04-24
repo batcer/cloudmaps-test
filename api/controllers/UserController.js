64d9ff561990765d2fb02d581a28a0a74a03a0b2
Mikhail Batcer <mbatser@yandex.ru>
2018-04-24 11:03:03 +0300

    Make UserController.request() more readable

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -317,50 +317,43 @@ module.exports = {
   },
 
   request: function(req, res){
-    if(req.xhr){
-      var id_requested = req.param('id_requested');
-      Request.count({
-        id_requesting: req.session.user.id,
-        id_requested: id_requested
-      }).exec(function(error, count){
-        if(error)
-          return res.negotiate(error);
-        else{
-          if(!count){
-            Request.create({
-              id_requesting: req.session.user.id,
-              id_requested: id_requested
-            }).exec(function(error, request){
-              if(error){
-                return res.send({
-                  success: false,
-                  error: error
-                });
-              }
-              else{
-                Request.findOne(request.id).populateAll().exec(function(error, request){
-                  request.id_requesting = _.omit(request.id_requesting, 'password');
-                  Request.publishCreate(request, req);
-                  return res.send({
-                    success: true,
-                    message: "Заявка успешно отправлена"
-                  });
-                });
-              }
-            });
-          }
-          else{
-            return res.send({
-              success: true,
-              message: "Заявка уже существует"
-            });
-          }
-        }
-      });
-    }
-    else{
+    if(!req.xhr) {
       return res.badRequest();
     }
+    var id_requested = req.param('id_requested');
+    Request.count({
+      id_requesting: req.session.user.id,
+      id_requested: id_requested
+    }).exec(function(error, count){
+      if(error){
+        return res.negotiate(error);
+      }
+      if(count){
+        return res.send({
+          success: true,
+          message: "Заявка уже существует"
+        });
+      }
+      Request.create({
+        id_requesting: req.session.user.id,
+        id_requested: id_requested
+      }).exec(function(error, request){
+        if(error){
+          return res.send({
+            success: false,
+            error: error
+          });
+        }
+        Request.findOne(request.id).populateAll().exec(function(error, request){
+          request.id_requesting = _.omit(request.id_requesting, 'password');
+          Request.publishCreate(request, req);
+          return res.send({
+            success: true,
+            message: "Заявка успешно отправлена"
+          });
+        });
+      });
+    });
   }
 };
 
